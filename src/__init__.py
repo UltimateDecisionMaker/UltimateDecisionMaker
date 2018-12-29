51912fabc278bc92455475f97fef396e91561d53
Raymond Ruazol <rpruazol@gmail.com>
2018-12-28 17:23:38 -0800

    changes

--- src/__init__.py
+++ src/__init__.py
@@ -15,5 +15,5 @@ app.config.from_mapping(
     SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL'),
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
-
+# test
 from . import auth, models, routes, forms, exceptions
