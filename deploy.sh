1416e664128c5e147f76d769018381bfa930ea72
Raymond Ruazol <rpruazol@gmail.com>
2019-01-02 11:45:20 -0800

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
+cd /home/ubuntu/UltimateDecisionMaker/
+echo "==========================="
 echo "Pulling from master..."
-echo "Pull from master completed!"
+echo "==========================="
+git reset --hard
+git pull origin master
+echo "==========================="
+echo "Pull from master successful!"
+echo "==========================="
+echo " "
+echo "==========================="
+echo "Upgrading/Migrating database..."
+echo "==========================="
+flask db upgrade
+flask db migrate
+echo " "
+echo "==========================="
+echo "Restarting gunicorn..."
+echo "==========================="
+sudo systemctl gunicorn restart
+echo " "
+echo " "
+echo "==========================="
+echo "Deploy completed"
+echo "==========================="
